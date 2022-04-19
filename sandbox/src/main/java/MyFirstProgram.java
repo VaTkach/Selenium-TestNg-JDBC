4c8a4deafe8c50927b176854949e39136180ce6a
VaTkach <vasektkach@gmail.com>
2022-04-19 16:27:00 +0300

    Just fun

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,17 @@
-class MyFirstProgram{
+class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь треугольника со стороной " + r.a + " и " + r.b + " = " + area(r));
+  }
+
+  public static double area(Square s) {
+    return s.l * s.l;
+  }
+
+  public static double area(Rectangle s) {
+    return s.a * s.b;
   }
 }
